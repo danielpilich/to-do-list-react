c241949a4184c4996114bb5ffb5077f622389176
Daniel Pilich <dpilich16@gmail.com>
2023-06-28 17:35:57 +0200

    Style buttons and tasks

--- client/src/components/ListItem.jsx
+++ client/src/components/ListItem.jsx
@@ -1,7 +1,21 @@
+import TickIcon from "./TickIcon"
+import ProgressBar from "./ProgressBar"
+
 function ListItem({task}) {
     return (
       <div className="list-item">
-        <p>{task.title}</p>
+        
+          <div className="info-container">
+            <TickIcon/>
+            <p className="task-title">{task.title}</p>
+            <ProgressBar/>
+          </div>
+
+          <div className="button-container">
+            <button className="button-edit">EDIT</button>
+            <button className="button-delete">DELETE</button>
+          </div>
+
       </div>
     )
   }
