f068e61f511b86cece5019c502b426b7cd84154b
Daniel Pilich <dpilich16@gmail.com>
2023-08-17 19:41:30 +0200

    Change the css code to improve the appearance of the app

--- client/src/components/ProgressBar.jsx
+++ client/src/components/ProgressBar.jsx
@@ -1,9 +1,6 @@
 function ProgressBar({progress}) {
   const colors = [
-    'rgb(255, 214, 161)',
-    'rgb(255, 175, 163)',
-    'rgb(108, 115, 148)',
-    'rgb(141, 181, 145)'
+    '#0fbcf9'
   ]
 
   const randomColor = colors[Math.floor(Math.random() * colors.length)]
